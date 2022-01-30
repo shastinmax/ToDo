ce4432bfced0a5e2a6fa9914615e0a3f630bd98f
Shastin Maksim <shastin_maksim@mail.ru>
2022-01-30 13:21:44 +0300

    add test changeTodolistTitle

--- src/reducers/TodolistReducer/todolist-reducer.test.ts
+++ src/reducers/TodolistReducer/todolist-reducer.test.ts
@@ -1,6 +1,6 @@
-import {addTodolistAC, removeTodolistReducerAC, todolistReducer} from './todolist-reducer';
+import {addTodolistAC, changeTodolistTitleAC, removeTodolistReducerAC, todolistReducer} from './todolist-reducer';
 import {v1} from 'uuid';
-import {TodolistType} from '../../App';
+import {FilterValuesType, TodolistType} from '../../App';
 
 test.skip('correct todolist should be removed', () => {
     let todolistId1 = v1();
@@ -18,7 +18,7 @@ test.skip('correct todolist should be removed', () => {
     expect(endState[0].id).toBe(todolistId2);
 });
 
-test('correct todolist should be added', () => {
+test.skip('correct todolist should be added', () => {
     let todolistId1 = v1();
     let todolistId2 = v1();
 
@@ -33,5 +33,44 @@ test('correct todolist should be added', () => {
 
     expect(endState.length).toBe(3);
     expect(endState[2].title).toBe(newTodolistTitle);
+});
+test.skip('correct todolist should change its name', () => {
+    let todolistId1 = v1();
+    let todolistId2 = v1();
+
+    let newTodolistTitle = "New Todolist";
+
+    const startState: Array<TodolistType> = [
+        {id: todolistId1, title: "What to learn", filter: "all"},
+        {id: todolistId2, title: "What to buy", filter: "all"}
+    ]
+
+
+    const endState = todolistReducer(startState, changeTodolistTitleAC(todolistId2,newTodolistTitle));
+
+    expect(endState[0].title).toBe("What to learn");
+    expect(endState[1].title).toBe(newTodolistTitle);
+});
+test('correct filter of todolist should be changed', () => {
+    let todolistId1 = v1();
+    let todolistId2 = v1();
+
+    let newFilter: FilterValuesType = "completed";
+
+    const startState: Array<TodolistType> = [
+        {id: todolistId1, title: "What to learn", filter: "all"},
+        {id: todolistId2, title: "What to buy", filter: "all"}
+    ]
+
+    const action = {
+        type: 'CHANGE-TODOLIST-FILTER',
+        id: todolistId2,
+        filter: newFilter
+    };
+
+    const endState = todolistReducer(startState, action);
+
+    expect(endState[0].filter).toBe("all");
+    expect(endState[1].filter).toBe(newFilter);
 });
 
--- src/reducers/TodolistReducer/todolist-reducer.ts
+++ src/reducers/TodolistReducer/todolist-reducer.ts
@@ -2,24 +2,28 @@ import {TodolistType} from "../../App";
 import {v1} from "uuid";
 
 export const todolistReducer=(state:Array<TodolistType>,action:GeneralType)=>{
+    let newState
     switch (action.type) {
         case "REMOVE-TODOLIST":
             // setTodolists(todolists.filter(tl => tl.id != id));
             // delete tasks[id];
             // setTasks({...tasks});
-            let newState=[...state]
+             newState=[...state]
             return newState.filter(f=>f.id!==action.payload.id)
         case "ADD-TODOLIST":
-            let newsState=[...state]
-
+             newState=[...state]
             let newTodolist: TodolistType = {id: v1(), title: action.payload.title, filter: 'all'};
-            return [...newsState,newTodolist]
+            return [...newState,newTodolist]
+        case'CHANGE-TODOLIST-TITLE':
+            let newsState=[...state]
+            return newsState.map(m=>m.id===action.payload.id?{...m,title:action.payload.title}:m)
         default:
             return state
     }
 }
 type GeneralType=TodolistReducerACType
 |AddTodolistAC
+|ChangeTodolistTitleACType
 export type TodolistReducerACType=ReturnType<typeof removeTodolistReducerAC>
 export const removeTodolistReducerAC=(id:string)=>{
     return{
@@ -36,5 +40,21 @@ export const addTodolistAC=(title:string)=>{
         payload:{
             title
         }
+    }as const
+}
+export type ChangeTodolistTitleACType=ReturnType<typeof changeTodolistTitleAC >
+export const changeTodolistTitleAC=(id: string, title: string)=>{
+    return{
+        type:'CHANGE-TODOLIST-TITLE',
+        payload:{
+            id,
+            title
+        }
+    }as const
+}
+
+export const changeFilter=()=>{
+    return{
+        type:
     }as const
 }
