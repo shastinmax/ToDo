3d426d70fddef2e2a160ab593d7476042d2d6105
Shastin Maksim <shastin_maksim@mail.ru>
2022-02-05 13:18:25 +0300

    Updates

---
+++ precache-manifest.e1957ebf6bb9e8d30b2a53558aa1e0a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ece2cfbea9f2292f34ad24b6f9a63bef",
+    "url": "/Social-Network-TypeScript/index.html"
+  },
+  {
+    "revision": "fe404cfb6fb50239da6d",
+    "url": "/Social-Network-TypeScript/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "ecb41a5f49bec60a887c",
+    "url": "/Social-Network-TypeScript/static/js/2.f40acb60.chunk.js"
+  },
+  {
+    "revision": "d5aac6574fac166201ec7de4469e2acd",
+    "url": "/Social-Network-TypeScript/static/js/2.f40acb60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe404cfb6fb50239da6d",
+    "url": "/Social-Network-TypeScript/static/js/main.a5103fb3.chunk.js"
+  },
+  {
+    "revision": "7b9ddefa8f0e2149e215",
+    "url": "/Social-Network-TypeScript/static/js/runtime-main.d68ad265.js"
+  }
+]);
