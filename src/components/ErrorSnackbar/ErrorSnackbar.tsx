b2724e84cfb006ae5b701c50d88c1516f11d5dd2
Shastin Maksim <shastin_maksim@mail.ru>
2022-03-28 14:28:01 +0300

    add formik and validation

--- src/components/ErrorSnackbar/ErrorSnackbar.tsx
+++ src/components/ErrorSnackbar/ErrorSnackbar.tsx
@@ -1,21 +1,23 @@
-import React, {useState} from 'react';
+import React from 'react';
 import Snackbar from '@mui/material/Snackbar';
-import MuiAlert, {AlertProps} from '@mui/material/Alert';
-import {useDispatch, useSelector} from "react-redux";
-import {AppRootStateType} from "../../app/store";
-import {NullableType, setAppErrorAC} from "../../app/app-reducer";
+import MuiAlert, { AlertProps } from '@mui/material/Alert';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppRootStateType } from '../../app/store';
+import { setAppErrorAC } from '../../app/app-reducer';
 
 const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
     props, ref) {
     return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
 });
 
-export function ErrorSnackbar() {
-    const error = useSelector<AppRootStateType, NullableType<string>>(state => state.app.error)
-    const dispatch = useDispatch()
-    const [open, setOpen] = useState(true);
 
-    const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
+export function ErrorSnackbar() {
+
+    const error = useSelector<AppRootStateType, string | null>(state => state.app.error)
+
+    const dispatch = useDispatch();
+
+    const handleClose = (event?: React.SyntheticEvent, reason?: string) => {
         if (reason === 'clickaway') {
             return;
         }
@@ -23,7 +25,7 @@ export function ErrorSnackbar() {
     };
 
     return (
-        <Snackbar open={!!error} autoHideDuration={6000} onClose={handleClose}>
+        <Snackbar open={error !== null} autoHideDuration={6000} onClose={handleClose}>
             <Alert onClose={handleClose} severity="error" sx={{width: '100%'}}>
                 {error}
             </Alert>
--- src/utils/error-utils.ts
+++ src/utils/error-utils.ts
@@ -1,14 +1,17 @@
-import {setAppErrorAC, setAppStatusAC} from "../app/app-reducer";
+import {setAppErrorAC, SetAppErrorActionType, setAppStatusAC, SetAppStatusActionType} from '../app/app-reducer'
+import {ResponseType} from '../api/todolists-api'
+import {Dispatch} from 'redux'
 
-export const handleServerAppError = (dispatch: any, message: string) => {
-    dispatch(setAppErrorAC(message))
-    dispatch(setAppStatusAC('failed'))
-}
-
-export const handleServerNetworkError = (dispatch: any, data: any) => {
+export const handleServerAppError = <D>(data: ResponseType<D>, dispatch: Dispatch<SetAppErrorActionType | SetAppStatusActionType>) => {
     if (data.messages.length) {
         dispatch(setAppErrorAC(data.messages[0]))
     } else {
-        dispatch(setAppErrorAC('Some error occured'))
+        dispatch(setAppErrorAC('Some error occurred'))
     }
+    dispatch(setAppStatusAC('failed'))
+}
+
+export const handleServerNetworkError = (error: { message: string }, dispatch: Dispatch<SetAppErrorActionType | SetAppStatusActionType>) => {
+    dispatch(setAppErrorAC(error.message ? error.message : 'Some error occurred'))
+    dispatch(setAppStatusAC('failed'))
 }
